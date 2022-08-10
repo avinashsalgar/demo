3af81c0a54dce4650f5e9d6cbd4244a071e08e50
avinash <avisalgar12@gmail.com>
2022-08-10 21:30:09 +0530

    [avinash]added:employe wage use case 9

---
+++ empwageuc9.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+parttime=1
+fulltime=2
+emprateperhour=20
+maxhoursmonth=10
+numworkingdays=20
+totalemphours=0
+totalworkingdays=0
+
+declare -A dailywage
+
+function workinghour() {
+
+ case $randomcheck in
+        $fulltime)
+        emphours=16
+        ;;
+        $parttime)
+        parttime=8
+        ;;
+        *)
+        emphours=0
+        ;;
+
+esac
+echo $emphours
+}
+while [[ $totalemphours -lt $maxhoursmonth && $totalworkingdays -lt $numworkingdays ]]
+
+do
+
+        ((totalworkingdays++))
+        randomcheck=$((RANDOM%3))
+         emphours=$( workinghour $randomcheck )
+        totalemphours=$(($totalemphours+$emphours))
+        dailywage["$totalworkingdays"]="$(($emphours*$emprateperhour))"
+done
+
+monthlywage=$(($totalemphours*$emprateperhour))
+
+echo ${dailywage[@]}
+echo ${!dailywage[@]}
+
+
+
+
+
